e60f565c1148322e2b1e554c4a17c1128db20a92
dino <dinotestes12@gmail.com>
2022-04-21 10:17:16 +0700

    hari kartini

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js jc20</code> and save to reload.
+          Edit <code>src/App.js jc20 kamis,21 april hari kartini</code> and save
+          to reload.
         </p>
         <a
           className="App-link"
